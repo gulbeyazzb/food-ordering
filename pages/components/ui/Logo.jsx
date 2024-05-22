34893266caf7b9d51f02eddf80210a04c95cec42
Gulbeyaz <glbzbayram@gmail.com>
2024-05-22 21:37:23 +0300

    Page's logo

---
+++ pages/components/ui/Logo.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <div className="hover:text-primary cursor-pointer text-[2rem] font-dancing font-bold pb-2">
+      Feane
+    </div>
+  );
+};
+
+export default Logo;
